19cb56fbdaeee1b9ad55cceda4a925b1a6e2ca27
Chidi <chidiamaraekejiuba@gmail.com>
2023-07-27 05:01:00 +0100

    solution update

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,20 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default function handleProfileSignup(firstName, lastName, filename) {
+    return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
+        .then((values) => (
+            values.map((value) => {
+                if (value.status === 'fulfilled') {
+                    return {
+                        status: value.status,
+                        value: value.value,
+                    };
+                }
+                return {
+                    status: value.status,
+                    value: `Error: ${value.reason.message}`,
+                };
+            }
+        )));
+}
