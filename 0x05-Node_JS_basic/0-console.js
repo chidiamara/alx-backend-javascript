ee62cc5f7836f095d1e65c6dbeb11b228cd12584
Chidi <chidiamaraekejiuba@gmail.com>
2023-09-23 05:26:48 +0100

    nodejs basics solution

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Chidiamara <https://github.com/chidiamara>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
